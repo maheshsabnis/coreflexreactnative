c9ce317f121b4e3bee59173ff0957ff1dca8369b
mahesh <sabnis_m@hotmail.com>
2023-11-24 12:01:28 +0530

    ddss

--- components/touchableopacitycomponent.js
+++ components/touchableopacitycomponent.js
@@ -1,17 +1,37 @@
 import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  ActivityIndicator,
+} from 'react-native';
 const TouchableOpacityComponent = () => {
   const [counter, setCounter] = useState(0);
   const increament = () => {
     setCounter(ct => ct + 1);
   };
+  /* require('./../imgs/first.png') */
+  const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
   return (
     <View style={styles.view}>
       <Text style={styles.text}>Using TouchableOpacity</Text>
       <Text style={styles.text}>Count: {counter}</Text>
       <TouchableOpacity style={styles.button} onPress={increament}>
         <Text style={styles.text}>Click</Text>
+        <ActivityIndicator />
       </TouchableOpacity>
+      <ImageBackground
+        source={require('./../imgs/first.png')}
+        resizeMode="cover"
+        style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+      <ActivityIndicator />
+    <ActivityIndicator size="large" />
+    <ActivityIndicator size="small" color="#0000ff" />
+    <ActivityIndicator size="large" color="#00ff00" />
     </View>
   );
 };
